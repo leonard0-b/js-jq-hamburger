3a6c2e849bf9c20724803b30d31de830dac6566d
Leonardo <leonardo.bongianni@gmail.com>
2021-03-10 16:10:10 +0100

    Due versioni

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+// mostrare / nascondere il menu principale (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+
+// individuo la variabile di cosa deve essere cliccato🐱‍👤
 var hamburger = $(".fa-bars");
 
+// creo variabili di cosa deve essere modificato🐱‍👤
 var hamburgerMenu = $(".hamburger-menu")
-
 var menuClose = $(".close");
 
 // VERSIONE 1🐱‍👤
@@ -14,10 +17,15 @@ var menuClose = $(".close");
 // });
 
 // VERSIONE 2🐱‍👤
+// al click su ".fa-bars" il ".hamburger-menu" diventa "active" (vedi css)🐱‍👤
 hamburger.click(function() {
+  // ".hamburger-menu" col toggle diventa active e quindi si mostra🐱‍👤
   hamburgerMenu.toggleClass("active");
 });
 
+// al secondo click con toggle la classe "active" si rimuove🐱‍👤
 menuClose.click(function() {
   hamburgerMenu.toggleClass("active");
 });
+
+// grazie a toggleClass("active") ad ogni click la classe active viene inserita e si rimuove🐱‍👤
